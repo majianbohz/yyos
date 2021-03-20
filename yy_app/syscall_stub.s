c8098d6bba9af15713f046a0a68218fa63b79e02
majianbo <641137800@qq.com>
2021-03-20 19:41:45 +0800

    no logic update

--- yy_app/syscall_stub.s
+++ yy_app/syscall_stub.s
@@ -6,19 +6,6 @@
 global syscall ;
 
 syscall:
-  
   int 0x80 ;
-  
-;  mov ebp, esp
-;  mov esi, [ebp + 16] ; pout
-;  mov ecx, [ebp + 20] ; outsize
-;  shr ecx, 2  ; 4-bytes  32bits
-  
-;.label_syscall:
-;  pop eax
-;  mov [esi], eax
-;  shl esi, 2
-;  loop .label_syscall
-  
   ret
   
